0da802de1a0cec8b91b25aaa5af21258c515e2de
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-09-23 10:41:29 -0500

    fixed api-gateway and added docker-compose

--- api-gateway/src/main/java/com/campusretail/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/campusretail/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,19 @@
 package com.campusretail.apigateway;
 
+import com.campusretail.apigateway.config.SpringSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {SpringSecurityConfiguration.class}
+        )
+})
 @EnableEurekaClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
