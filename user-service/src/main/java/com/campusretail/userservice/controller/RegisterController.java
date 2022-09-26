a964c0235378966bceaf90d6977d4e9f73fd6ebc
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-09-26 14:12:58 -0500

    Added docker datasource and driver

--- user-service/src/main/java/com/campusretail/userservice/controller/RegisterController.java
+++ user-service/src/main/java/com/campusretail/userservice/controller/RegisterController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller that registers into the database the wanted user
  */
 @RestController
+@EnableAsync
 public class RegisterController {
 
 	private final UserService userService;
