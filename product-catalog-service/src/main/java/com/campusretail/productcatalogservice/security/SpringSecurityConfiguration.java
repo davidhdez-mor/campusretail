6f1a185f60646a29984d4c5e837385f94a2dfda9
DavidHernandezMorones <david.2.hernandez@atos.net>
2022-09-27 11:34:40 -0500

    Implemented security: authorization and authentication

---
+++ product-catalog-service/src/main/java/com/campusretail/productcatalogservice/security/SpringSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.campusretail.productcatalogservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.httpBasic(Customizer.withDefaults())
+				.csrf()
+				.disable()
+				.authorizeHttpRequests()
+					.antMatchers("/admin/**").hasRole("ADMIN")
+					.antMatchers("/**").permitAll()
+				.and()
+				.formLogin();
+		return http.build();
+	}
+}
